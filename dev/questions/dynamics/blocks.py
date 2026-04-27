45531ddf04aebd2b9c4b4559e756378f23914641
the-ethan-wang <ethanwang314159@gmail.com>
2026-04-27 11:12:02 +1000

    bugfix

--- dev/questions/dynamics/blocks.py
+++ dev/questions/dynamics/blocks.py
@@ -41,6 +41,8 @@ def generate_block_q():
     dir_from = "left" if net_force>0 else "right"
     filled_question = question_base.format(*[number_of_blocks, abs(net_force), block_exerted_on, dir_from])
 
+    marks = 6
+    mark_distribution = [1, 1, 2, 2]
     acceleration_answer = "The acceleration of the system is {0}ms^-2".format(system_acceleration)
     each_acc_answer = "Each block has an acceleration of {0}ms^-2".format(system_acceleration)
     each_block_nf_base = "Block {0} has a net force of {1}N"
@@ -53,20 +55,15 @@ def generate_block_q():
     assert forces
     forces = forces["forces"]
     each_block_f_base = "{0} has {1} force{2} acting on it. They are: {3}"
-    each_force_base = "{0}N {1} force"
+    each_force_base = "{0}N {1}"
     each_block_forces_answer = []
     for force in forces:
         block_ans = each_block_f_base.format(force["name"], len(force["forces"]), "s" if len(force["forces"])!=1 else "", ", ".join([each_force_base.format(x[1],x[0]) for x in force["forces"]]))
         each_block_forces_answer.append(block_ans)
     each_block_forces_answer="\n".join(each_block_forces_answer)
     question_data = {
-        "has": {
-            "has_question": True,
-            "has_question_parts": True,
-            "has_answer": False,
-            "has_answer_parts": True,
-            "has_diagram_data": True
-        },
+        "marks": marks,
+        "mark_distribution": mark_distribution,
 
         "question": filled_question,
         "question_parts": question_parts,
@@ -97,40 +94,57 @@ def block_forces(weights: list[Union[float,int]], net_force: Optional[Union[floa
         return
 
     total_mass=sum(weights)
-    if net_acceleration and net_force:
+
+    if net_acceleration is not None and net_force is not None:
         if net_force!=net_acceleration*total_mass:
             print("Net force conflicts with net acceleration")
             return
-    elif net_acceleration:
-        net_force = net_acceleration*total_mass
-    elif net_force:
-        net_acceleration = net_force/total_mass
+    elif net_acceleration is not None:
+        net_force=net_acceleration*total_mass
+    elif net_force is not None:
+        net_acceleration=net_force/total_mass
     else:
         print("Not enough information")
         return
     
     ans={"forces": [], 'net acceleration': net_acceleration, 'net force': net_force}
+    n=len(weights)
+    net_forces=[net_acceleration*x for x in weights]
+    contacts=[]
+    for i in range(n - 1):
+        right_mass=sum(weights[i+1:])
+        contact_force=right_mass * net_acceleration
+        contacts.append(contact_force)
     for i, weight in enumerate(weights):
-        obj_net_force = net_acceleration*weight
-        obj_data={"name": f"Block {i+1}", "weight": weight, "forces": [], "net force": obj_net_force}
+        obj_net_force=net_forces[i]
+        obj_data = {
+            "name": f"Block {i+1}",
+            "weight": weight,
+            "forces": [],
+            "net force": obj_net_force
+        }
         if i==0:
             if net_force>=0:
                 obj_data["forces"].append(["Applied", net_force])
-                obj_data["forces"].append(["Contact Reaction", obj_net_force-net_force])
+                if n>1:
+                    obj_data["forces"].append([f"Contact force (from Block {i+2})", -contacts[0]])
             else:
-                obj_data["forces"].append(["Contact", obj_net_force])
-        elif i==len(weights)-1:
+                if n>1:
+                    obj_data["forces"].append([f"Contact force (from Block {i+2})", obj_net_force])
+        elif i==n-1:
             if net_force<0:
                 obj_data["forces"].append(["Applied", net_force])
-                obj_data["forces"].append(["Contact Reaction", obj_net_force-net_force])
+                obj_data["forces"].append([f"Contact force (from Block {i})", -contacts[i-1]])
             else:
-                obj_data["forces"].append(["Contact", obj_net_force])
+                obj_data["forces"].append([f"Contact force (from Block {i})", contacts[i-1]])
         else:
-            obj_data["forces"].append(["Contact", -1*ans['forces'][i-1]['forces'][-1][1]])
-            obj_data["forces"].append(["Contact Reaction", obj_net_force-obj_data['forces'][0][1]])
-        ans['forces'].append(obj_data)
-    return ans    
-
+            left_contact=contacts[i-1]
+            right_contact=contacts[i]
+            obj_data["forces"].append([f"Contact force (from Block {i})", left_contact])
+            obj_data["forces"].append([f"Contact force (from Block {i+2})", -right_contact])
+        ans["forces"].append(obj_data)
+    print(contacts)
+    return ans
 
 
 a = generate_block_q()
